95ea13a93162af6655a62093a5200ef2f1e00461
jimmy nagles <jimmynagles@gmail.com>
2020-08-12 17:27:03 -0700

    input style

--- src/components/Form/MyForm.js
+++ src/components/Form/MyForm.js
@@ -1,18 +1,18 @@
 import React, { useState } from "react";
 import { Col, Row } from "react-materialize";
-
+import "../../App.css";
 const MyForm = (props) => {
   const [food, setfood] = useState("");
 
   return (
     <div className="container ">
       <Row>
-        <Col s={6} m={6} l={6}>
+        <Col s={9} m={10} l={10}>
           <form>
-            <input></input>
+            <input placeholder="Search Recipe" className="white-text"></input>
           </form>
         </Col>
-        <Col s={6} m={6} l={6}>
+        <Col s={3} m={2} l={2}>
           <div className="right"> {props.children}</div>
         </Col>
       </Row>
