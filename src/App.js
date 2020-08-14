f0b413fc4c14490f5b95805f2ecf93ce4161d68f
jimmy nagles <jimmynagles@gmail.com>
2020-08-13 19:10:40 -0700

    api

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import { useSpring, animated } from "react-spring";
 import MyNav from "./components/Nav/MyNav";
 import { Button, Container, Row } from "react-materialize";
 import MyForm from "./components/Form/MyForm";
-import API from "./utils/API";
-import RecipeCard from "./components/Card/RecipeCard";
 
 import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
 
@@ -18,36 +16,10 @@ function App() {
       : "translate3d(100%,0,0) scale(0.3)",
   });
 
-  const [result, setResult] = useState({});
-  const [search, setSearch] = useState("");
-
-  const SearchFood = (query) => {
-    API.search(query)
-      .then((res) => setResult(res.data))
-      .catch((err) => console.log(err));
-  };
-
-  const handleInputChange = (event) => {
-    setSearch(event.target.value);
-  };
-
-  const handleFormSubmit = (event) => {
-    event.preventDefault();
-    console.log(process.env.REACT_APP_SPOON);
-
-    SearchFood(search);
-    console.log("result", result);
-    setSearch("");
-  };
-
   return (
     <animated.div className="backg">
       <AmplifySignOut />
-      <MyForm
-        value={search}
-        handleFormSubmit={handleFormSubmit}
-        handleInputChange={handleInputChange}
-      >
+      <MyForm>
         <Button
           large
           className="btn-flat transparent white-text"
@@ -55,23 +27,11 @@ function App() {
             setNavOpen(!isNavOpen);
           }}
         >
+          {" "}
           Menu
         </Button>
       </MyForm>
 
-      <Container>
-        <Row>
-          {result.recipes &&
-            result.recipes.map((recipe) => (
-              <RecipeCard
-                image={recipe.image}
-                key={recipe.id}
-                title={recipe.title}
-                link={recipe.sourceUrl}
-              ></RecipeCard>
-            ))}
-        </Row>
-      </Container>
       <MyNav style={navAni} />
     </animated.div>
   );
--- src/components/Form/MyForm.js
+++ src/components/Form/MyForm.js
@@ -5,6 +5,30 @@ import "../../App.css";
 import RecipeCard from "../Card/RecipeCard";
 
 const MyForm = (props) => {
+
+
+  // const [result, setResult] = useState({});
+  // const [search, setSearch] = useState("");
+
+  // const SearchFood = (query) => {
+  //   API.search(query)
+  //     .then((res) => setResult(res.data))
+  //     .catch((err) => console.log(err));
+  // };
+
+  // const handleInputChange = (event) => {
+  //   setSearch(event.target.value);
+  // };
+
+  // const handleFormSubmit = (event) => {
+  //   event.preventDefault();
+  //   console.log(process.env.REACT_APP_SPOON);
+
+  //   SearchFood(search);
+  //   console.log("result", result);
+  //   setSearch("");
+  // };
+
   return (
     <div className="container ">
       <Row>
